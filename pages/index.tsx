0f7b457f43be96f16d9f972b2eff3d69cfc3560a
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-12-30 19:46:02 -0600

    continuando con el proyecto agregando NextUi para la interfaz

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from '@next/font/google'
+import {Button} from '@nextui-org/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -6,6 +7,7 @@ export default function HomePage() {
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Button color='secondary' shadow bordered>Click Me</Button>
     </>
   )
 }
